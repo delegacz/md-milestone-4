155c06c9131363a14f154e6c192d9cda17072331
Mateusz Delegacz <deli@Mateuszs-MacBook-Pro.local>
2019-12-25 19:49:04 +0000

    cart functionality added

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from .views import (
     HomeView,
-    ItemDetailView
+    ItemDetailView,
+    add_to_cart,
+    remove_from_cart
 )
 
 app_name ='ecommerce'
 
 urlpatterns = [
     path('',HomeView.as_view(), name='home'),
-    path('product/<slug>/', ItemDetailView.as_view(), name='product')
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart')
 ]
