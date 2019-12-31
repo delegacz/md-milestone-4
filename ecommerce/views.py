4d0e63a2ae3fc0855fc5041f913884408e8dbb04
Mateusz Delegacz <deli@Mateuszs-MacBook-Pro.local>
2019-12-31 15:11:29 +0000

    order summart/cart page and functionality added

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,9 @@
 from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View 
 from .models import Item, Order, OrderItem
 from django.shortcuts import redirect
 from django.utils import timezone
@@ -12,11 +15,25 @@ class HomeView(ListView):
     paginate_by = 8
     template_name = "home.html"
 
+class OrderSummaryView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context  = {
+                'object':order
+            }
+            return render(self.request, 'order_summary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, "You do not have an active order")
+            return redirect("/")
+
+        
+
 class ItemDetailView(DetailView):
     model = Item
     template_name = "product.html"
 
-
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_item, created = OrderItem.objects.get_or_create(
@@ -44,7 +61,7 @@ def add_to_cart(request, slug):
             messages.info(request, "This item was added to your cart.")
             return redirect("ecommerce:product", slug=slug)
             
-
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_qs = Order.objects.filter(
