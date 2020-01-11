2509beb28bb7a594b6b9faf813b828a82cb22b3d
Mateusz Delegacz <deli@Mateuszs-MacBook-Pro.local>
2020-01-11 13:57:48 +0000

    stripes keys added

--- main/settings.py
+++ main/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import os.path
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -170,5 +171,5 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 #Crispy Forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-STRIPE_SECRET_KEY = ''
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
