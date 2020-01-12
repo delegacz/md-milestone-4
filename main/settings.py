1adc813eadf02c429e58efd52f161bcb96cc5032
Mateusz Delegacz <deli@Mateuszs-MacBook-Pro.local>
2020-01-12 15:40:01 +0000

    AWS Media file storage implemented

--- main/settings.py
+++ main/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'django_countries',
     'debug_toolbar',
-
+    'storages',
 
     'ecommerce'
 ]
@@ -135,11 +135,29 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+#AWS CONFIG
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires':'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl':'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = 'md-milestone-4'
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+# Static files (CSS, JavaScript, Images
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media_root')
