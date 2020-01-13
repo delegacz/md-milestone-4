6ce3e52b79c50e0da027ec4f2dd9c68934bc7a75
Mateusz Delegacz <deli@Mateuszs-MacBook-Pro.local>
2020-01-13 22:44:24 +0000

    setting.py updates /allowed host for heroku. heroku push 04

--- main/settings.py
+++ main/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['md-milestone-project-4.herokuapp.com']
 
 # Application definition
 
