14bb6d494ddf8fe1e1de6a5c649a0b42436ad98d
Mateusz Delegacz <deli@Mateuszs-MacBook-Pro.local>
2020-01-29 21:48:56 +0000

    disabling django debug heroku push

--- main/settings.py
+++ main/settings.py
@@ -13,7 +13,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['md-milestone-project-4.herokuapp.com',
                  '127.0.0.1']
